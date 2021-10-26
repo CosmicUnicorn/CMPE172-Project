88fe1320e5b2ec5b49d47c62fbf9b327a11546c8
Jamie Taylor <james.taylor@sjsu.edu>
2021-10-25 18:33:08 -0700

    Created object classes

---
+++ app/student.py
@@ -0,0 +1,4 @@
+class Employee:
+    def __init__(self, name, enrollDate):
+        self.name = name
+        self.enrollDate = enrollDate
---
+++ app/worksheet.py
@@ -0,0 +1,5 @@
+class Worksheet:
+    def __init__(self, title, difficulty, subject):
+        self.title = title
+        self.difficulty = difficulty
+        self.subject = subject
